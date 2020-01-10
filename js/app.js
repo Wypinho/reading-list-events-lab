49f30149d772fa9d32086bd22d79ce90ecef9c7e
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-10 11:24:07 +0000

    aaarrrgh - forgot all about git

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form')
+  form.addEventListener('submit', handleFormSubmission)
+
 })
+
+const handleFormSubmission = function (event) {
+  event.preventDefault();
+  const book = document.createElement('li')
+  book.textContent = `Title: ${event.target.title.value},
+                      Author: ${event.target.author.value},
+                      Category; ${event.target.category.value}`
+
+  const list = document.querySelector('ul#reading-list');
+  list.appendChild(book)
+
+  // const form = document.querySelector('#new-item-form')
+  event.target.reset()
+
+}
+
+
+console.log("end of code");
